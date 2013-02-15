1d767c0ba813f81decc7eb7c4b212cbed99d251b
vasilkrvasilev <vasilkrvasilev@gmail.com>
2013-02-15 11:31:08 +0200

    add SearchAlgorithms and BitRotation

---
+++ BlogPosts/BitRotation/BitRotation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+class BitRotation
+{
+    static void PrintResult(bool isNumber, int number)
+    {
+        Console.Write("The created symbol is: ");
+        if (isNumber)
+        {
+            Console.WriteLine(number);
+        }
+        else
+        {
+            Console.WriteLine((char)number);
+        }
+
+        Console.WriteLine(Convert.ToString(number, 2).PadLeft(32, '0'));
+    }
+
+    static int RotateBits(int number, byte firstBeginningPosition, byte secondBeginningPosition, byte range)
+    {
+        byte distance = (byte)(secondBeginningPosition - firstBeginningPosition);
+        for (int bit = firstBeginningPosition; bit < (firstBeginningPosition + range); bit++)
+        {
+            int firstMask = 1 << bit;
+            int firstMaskNumber = number & firstMask;
+            byte firstBitValue = (byte)(firstMaskNumber >> bit);                  //0 or 1
+            int secondMask = 1 << (bit + distance);
+            int secondMaskNumber = number & firstMask;
+            byte secondBitValue = (byte)(firstMaskNumber >> (bit + distance));    //0 or 1
+            if (firstBitValue == 1)
+            {                                                                     //Change bit (i + range) to 1
+                int changeNumber = SetBitToOne((byte)(bit + distance), number);
+                if (secondBitValue == 1)
+                {                                                                 //Change bit i to 1
+                    number = SetBitToOne((byte)bit, changeNumber);
+                }
+                else
+                {                                                                 //Change bit i to 0
+                    number = SetBitToZero((byte)bit, changeNumber);
+                }
+            }
+            else
+            {                                                                     //Change bit (i + range) to 0
+                int changeNumber = SetBitToZero((byte)(bit + distance), number);
+                if (secondBitValue == 1)
+                {                                                                 //Change bit i to 1
+                    number = SetBitToOne((byte)bit, changeNumber);
+                }
+                else
+                {                                                                //Change bit i to 0
+                    number = SetBitToZero((byte)bit, changeNumber);
+                }
+            }
+        }
+
+        return number;
+    }
+
+    static int SetBitToOne(byte position, int number)
+    {
+        int changeMask = 1 << position;
+        int changeNumber = number | changeMask;
+        return changeNumber;
+    }
+
+    static int SetBitToZero(byte position, int number)
+    {
+        int changeMask = ~(1 << position);
+        int changeNumber = number & changeMask;
+        return changeNumber;
+    }
+
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("This program exchange the bits in the intervals");
+        Console.WriteLine("[first beginning position; first beginning position + range)");
+        Console.WriteLine("and\n[second beginning position; second beginning position + range)");
+        Console.WriteLine("Enter symbol");
+
+        int number;
+        string input = Console.ReadLine();
+        bool isNumber = int.TryParse(input, out number);
+        if (!isNumber)
+        {
+            number = (int)input[0];
+        }
+
+        Console.WriteLine(Convert.ToString(number, 2).PadLeft(32, '0'));
+
+        Console.WriteLine("Enter first beginning position");
+        byte firstBeginningPosition = byte.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second beginning position");
+        byte secondBeginningPosition = byte.Parse(Console.ReadLine());
+        Console.WriteLine("Enter range");
+        byte range = byte.Parse(Console.ReadLine());
+
+        number = RotateBits(number, firstBeginningPosition, secondBeginningPosition, range);
+
+        PrintResult(isNumber, number);
+    }
+}
---
+++ BlogPosts/SearchAlgorithms/SearchAlgorithms.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SearchAlgorithms
+{
+    static int[] GenerateArray(int number)
+    {
+        Console.WriteLine("Enter the number of elements in the array");
+        int elements = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the min value");
+        int downLimit = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the max value");
+        int upperLimit = int.Parse(Console.ReadLine());
+
+        int[] array = new int[elements];
+        Random randomGenerator = new Random();
+
+        for (int position = 0; position < elements; position++)
+        {
+            array[position] = randomGenerator.Next(downLimit + position, upperLimit);
+        }
+
+        for (int position = 0; position < elements; position++)
+        {
+            Console.Write("{0} ", array[position]);
+        }
+
+        Console.WriteLine();
+        return array;
+    }
+
+    static void LinearSearch(int[] array, int number)
+    {
+        int linearIterations = 0;
+        for (int position = 0; position < array.Length; position++)
+        {
+            linearIterations++;
+            if (array[position] == number)
+            {
+                Console.WriteLine("{0} is the {1} element of the array", number, position + 1);
+                Console.WriteLine("We did {0} iteration", linearIterations);
+                break;
+            }
+            if ((position == (array.Length - 1)) && (array[position] != number))
+            {
+                Console.WriteLine("There is no {0} in the array", number);
+                Console.WriteLine("We did {0} iteration", linearIterations);
+            }
+        }
+    }
+
+    static void BinarySearch(int[] array, int number)
+    {
+        Array.Sort(array);
+        int elements = array.Length;
+        int binaryIterations = 0;
+        bool noNumber = true;
+        int firstElement = 0;
+        int endElement = elements - 1;
+        int middleElement = elements / 2;
+
+        while (elements > 0)
+        {
+            binaryIterations++;
+            if (number == array[middleElement])
+            {
+                Console.WriteLine("{0} is on {1} position in the sorted array (from 0)", number, middleElement);
+                Console.WriteLine("We did {0} iteration", binaryIterations);
+                noNumber = false;
+                break;
+            }
+            else if (number < array[middleElement])
+            {
+                if (firstElement != middleElement)
+                {
+                    endElement = middleElement - 1;
+                    elements = middleElement - firstElement;
+                    middleElement = firstElement + elements / 2;
+                }
+                else
+                {
+                    elements = 0;
+                }
+            }
+            else
+            {
+                if (firstElement != middleElement)
+                {
+                    firstElement = middleElement + 1;
+                    elements = endElement - middleElement;
+                    middleElement = firstElement + elements / 2;
+                }
+                else
+                {
+                    elements = 0;
+                }
+            }
+        }
+        if (noNumber)
+        {
+            Console.WriteLine("There is no {0} in the array", number);
+            Console.WriteLine("We did {0} iteration", binaryIterations);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the number");
+        int number = int.Parse(Console.ReadLine());
+
+        int[] array = GenerateArray(number);
+
+        LinearSearch(array, number);
+
+        BinarySearch(array, number);
+    }
+}
